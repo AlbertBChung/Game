d9be2773e43f6333269bcd48da1a3e94942a54f8
Hobin <hobintiger@gmail.com>
2017-06-26 21:09:53 -0400

    can now export save state schema

--- models/saveState.js
+++ models/saveState.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 
 var saveStateSchema = new mongoose.Schema({
     id: Number,
-    inventory: [{ item: String }],
+    inventory: [{ item: String, itemCount: Number }],
     timePlayed: Number,
     npc: [{ npcName: String, interactionCount: Number }]
 })
+
+module.exports = mongoose.model('SaveState', saveStateSchema );
